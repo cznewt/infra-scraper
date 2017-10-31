9a0a3470b6814e223ed1090348ee41d318556459
Ales Komarek <ales.komarek@newt.cz>
2017-10-31 22:06:49 +0100

    More AWS EC2 resources

--- infra_scraper/input/amazon.py
+++ infra_scraper/input/amazon.py
@@ -17,11 +17,29 @@ class AmazonWebServicesInput(BaseInput):
             'name': 'Bucket',
             'icon': 'fa:hdd-o',
         },
+        'ec2_elastic_ip': {
+            'resource': 'AWS::EC2::EIP',
+            'client': 'ec2',
+            'name': 'Elastic IP',
+            'icon': 'fa:cube',
+        },
+        'ec2_elastic_ip_association': {
+            'resource': 'AWS::EC2::EIPAssociation',
+            'client': 'ec2',
+            'name': 'Elastic IP Association',
+            'icon': 'fa:cube',
+        },
         'ec2_instance': {
             'resource': 'AWS::EC2::Instance',
             'client': 'ec2',
             'name': 'Instance',
             'icon': 'fa:server',
+        },
+        'ec2_internet_gateway': {
+            'resource': 'AWS::EC2::InternetGateway',
+            'client': 'ec2',
+            'name': 'Internet Gateway',
+            'icon': 'fa:cube',
         },
         'ec2_key_pair': {
             'resource': 'AWS::EC2::KeyPair',
@@ -41,6 +59,12 @@ class AmazonWebServicesInput(BaseInput):
             'name': 'VPC',
             'icon': 'fa:cubes',
         },
+        'ec2_vpc_gateway_attachment': {
+            'resource': 'AWS::EC2::VPCGatewayAttachment',
+            'client': 'ec2',
+            'name': 'VPC Gateway Attachment',
+            'icon': 'fa:cube',
+        },
     }
 
     def __init__(self, **kwargs):
@@ -51,10 +75,12 @@ class AmazonWebServicesInput(BaseInput):
         self.s3_client = boto3.resource('s3')
 
     def scrape_all_resources(self):
+#        self.scrape_ec2_elastic_ips()
+        self.scrape_ec2_instances()
+        self.scrape_ec2_internet_gateways()
         self.scrape_ec2_subnets()
         self.scrape_ec2_vpcs()
         self.scrape_ec2_key_pairs()
-        self.scrape_ec2_instances()
         self.scrape_s3_buckets()
 
     def _create_relations(self):
@@ -80,6 +106,16 @@ class AmazonWebServicesInput(BaseInput):
                         resource_id,
                         resource['metadata']['SubnetId'])
 
+    def scrape_ec2_elastic_ips(self):
+        for item in self.ec2_client.eips.all():
+            resource = item.meta.__dict__
+            resource.pop('resource_model')
+            resource.pop('client')
+            print resource
+            self._scrape_resource(resource['data']['InternetGatewayId'],
+                                  resource['data']['InternetGatewayId'],
+                                  'ec2_internet_gateway', None, metadata=resource['data'])
+
     def scrape_ec2_instances(self):
         for item in self.ec2_client.instances.all():
             resource = item.meta.__dict__
@@ -92,6 +128,15 @@ class AmazonWebServicesInput(BaseInput):
             self._scrape_resource(resource['data']['InstanceId'],
                                   name,
                                   'ec2_instance', None, metadata=resource['data'])
+
+    def scrape_ec2_internet_gateways(self):
+        for item in self.ec2_client.internet_gateways.all():
+            resource = item.meta.__dict__
+            resource.pop('resource_model')
+            resource.pop('client')
+            self._scrape_resource(resource['data']['InternetGatewayId'],
+                                  resource['data']['InternetGatewayId'],
+                                  'ec2_internet_gateway', None, metadata=resource['data'])
 
     def scrape_ec2_key_pairs(self):
         for item in self.ec2_client.key_pairs.all():

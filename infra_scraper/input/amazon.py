f02132fd10b9c2315bb2372b2238649b2a27e99f
Ales Komarek <ales.komarek@newt.cz>
2017-10-31 21:08:50 +0100

    Basic AWS EC2 resource

--- infra_scraper/input/amazon.py
+++ infra_scraper/input/amazon.py
@@ -12,16 +12,34 @@ class AmazonWebServicesInput(BaseInput):
 
     RESOURCE_MAP = {
         's3_bucket': {
-            'resource': 's3_bucket',
+            'resource': 'AWS::S3::Bucket',
             'client': 's3',
-            'name': 'S3 Bucket',
+            'name': 'Bucket',
             'icon': 'fa:hdd-o',
         },
         'ec2_instance': {
-            'resource': 'ec2_instance',
+            'resource': 'AWS::EC2::Instance',
             'client': 'ec2',
-            'name': 'EC2 Instance',
+            'name': 'Instance',
             'icon': 'fa:server',
+        },
+        'ec2_key_pair': {
+            'resource': 'AWS::EC2::KeyPair',
+            'client': 'ec2',
+            'name': 'Key Pair',
+            'icon': 'fa:key',
+        },
+        'ec2_subnet': {
+            'resource': 'AWS::EC2::Subnet',
+            'client': 'ec2',
+            'name': 'Subnet',
+            'icon': 'fa:cube',
+        },
+        'ec2_vpc': {
+            'resource': 'AWS::EC2::VPC',
+            'client': 'ec2',
+            'name': 'VPC',
+            'icon': 'fa:cubes',
         },
     }
 
@@ -33,20 +51,78 @@ class AmazonWebServicesInput(BaseInput):
         self.s3_client = boto3.resource('s3')
 
     def scrape_all_resources(self):
+        self.scrape_ec2_subnets()
+        self.scrape_ec2_vpcs()
+        self.scrape_ec2_key_pairs()
         self.scrape_ec2_instances()
         self.scrape_s3_buckets()
 
     def _create_relations(self):
-        pass
+        for resource_id, resource in self.resources.get('ec2_instance', {}).items():
+            if 'VpcId' in resource['metadata']:
+                if resource['metadata']['VpcId'] in self.resources.get('ec2_vpc', {}):
+                    self._scrape_relation(
+                        'ec2_instance-ec2_vpc',
+                        resource_id,
+                        resource['metadata']['VpcId'])
+
+            if 'KeyName' in resource['metadata']:
+                if resource['metadata']['KeyName'] in self.resources.get('ec2_key_pair', {}):
+                    self._scrape_relation(
+                        'ec2_instance-ec2_key_pair',
+                        resource_id,
+                        resource['metadata']['KeyName'])
+
+            if 'SubnetId' in resource['metadata']:
+                if resource['metadata']['SubnetId'] in self.resources.get('ec2_subnet', {}):
+                    self._scrape_relation(
+                        'ec2_instance-ec2_subnet',
+                        resource_id,
+                        resource['metadata']['SubnetId'])
 
     def scrape_ec2_instances(self):
         for item in self.ec2_client.instances.all():
             resource = item.meta.__dict__
             resource.pop('resource_model')
             resource.pop('client')
+            try:
+                name =  resource['data']['NetworkInterfaces'][0]['Association']['PublicDnsName']
+            except Exception:
+                name = resource['data']['InstanceId']
             self._scrape_resource(resource['data']['InstanceId'],
-                                  resource['data']['InstanceId'],
-                                  'ec2_instance', None, metadata=resource)
+                                  name,
+                                  'ec2_instance', None, metadata=resource['data'])
+
+    def scrape_ec2_key_pairs(self):
+        for item in self.ec2_client.key_pairs.all():
+            resource = item.meta.__dict__
+            resource.pop('resource_model')
+            resource.pop('client')
+            self._scrape_resource(resource['data']['KeyName'],
+                                  resource['data']['KeyName'],
+                                  'ec2_key_pair', None, metadata=resource['data'])
+
+    def scrape_ec2_subnets(self):
+        for item in self.ec2_client.subnets.all():
+            resource = item.meta.__dict__
+            resource.pop('resource_model')
+            resource.pop('client')
+            self._scrape_resource(resource['data']['SubnetId'],
+                                  resource['data']['SubnetId'],
+                                  'ec2_subnet', None, metadata=resource['data'])
+
+    def scrape_ec2_vpcs(self):
+        for item in self.ec2_client.vpcs.all():
+            resource = item.meta.__dict__
+            resource.pop('resource_model')
+            resource.pop('client')
+            name = resource['data']['VpcId']
+            for tag in resource['data'].get('Tags', {}):
+                if tag['Key'] == 'Name':
+                    name = tag['Value']
+            self._scrape_resource(resource['data']['VpcId'],
+                                  name,
+                                  'ec2_vpc', None, metadata=resource['data'])
 
     def scrape_s3_buckets(self):
         for item in self.s3_client.buckets.all():
@@ -55,4 +131,4 @@ class AmazonWebServicesInput(BaseInput):
             resource.pop('client')
             self._scrape_resource(resource['data']['Name'],
                                   resource['data']['Name'],
-                                  's3_bucket', None, metadata=resource)
+                                  's3_bucket', None, metadata=resource['data'])

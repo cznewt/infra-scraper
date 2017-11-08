d029128be76e5109426c903fb9a8e99cb1b4fcdb
Michael Kutý <6du1ro.n@gmail.com>
2017-11-08 14:55:58 +0100

    Improve CLI and support debug option.

--- infra_scraper/main.py
+++ infra_scraper/main.py
@@ -90,10 +90,12 @@ class InfraScraper(object):
     def scrape_all_data(self):
         config = self.get_global_config()
         for endpoint_name, endpoint in config['endpoints'].items():
+            if config.get('debug', False):
+                return self.scrape_data(endpoint_name)
             try:
                 self.scrape_data(endpoint_name)
             except Exception as e:
-                logger.error('Scraping endpoing {} failed with error: {}'.format(endpoint_name, e))
+                logger.error('Scraping endpoint {} failed with error: {}'.format(endpoint_name, e))
 
     def scrape_data_forever(self, name, interval):
         config = self.get_global_config()
